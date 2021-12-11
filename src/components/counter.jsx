d3bdbe29ebe55edf4810374bb56c03c33beaed64
Genius AbuShalehMD <genius@lowp.jp>
2021-12-11 15:12:06 +0900

    Added component

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        {" "}
+        <h1 className="text-center"> Hello World </h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
