708fbf398ead02e4a587459b58d9beccf2b80303
Genius AbuShalehMD <genius@lowp.jp>
2021-12-14 01:49:34 +0900

    Link added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to my test app....</p>
+        <p>
+          <a href="#">Link added</a>
+        </p>
       </header>
     </div>
   );
