9d42d9901a74dbf9fc4d2e6dcf61d92671a85f6e
Genius AbuShalehMD <genius@lowp.jp>
2021-12-14 13:12:49 +0900

    Added app link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to my test app....</p>
-        <p>fake link added.......</p>
+        <p>
+          <a href="http://www.ec2-54-168-201-149.ap-northeast-1.compute.amazonaws.com">
+            AWS EC2 server link added.......
+          </a>
+        </p>
       </header>
     </div>
   );
